be61283db48f852087ac6b038647a7a7856175dd
Yoon <organicferrum@gmail.com>
2021-05-22 13:03:23 +0900

    게시글 조회 작성

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,14 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 //JPA에서는 Repository라고 부르며 인터페이스로 생서
 //Entity 클래스와 기본 ENtity repository는 함께 도메인에 위치해야한다.
 //상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
 public interface PostsRepository extends JpaRepository<Posts, Long>{
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
